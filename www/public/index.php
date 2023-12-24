73013f90532449a3d72ea076a25287b104ffb220
buryk20 <burykseriy@gmail.com>
2023-12-24 23:44:52 +0200

    ball

--- www/public/index.php
+++ www/public/index.php
@@ -1,2 +1,18 @@
-<?php
-echo "<h1>hello world</h1>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="assets/main.css">
+</head>
+<body>
+<main data-wrp-main-page class="wrp">
+    <div data-ball class="ball"></div>
+    <?php
+    echo "<h1 class='title'>Hello world</h1>";
+    ?>
+</main>
+
+<script src="assets/index.js"></script>
+</body>
+</html>

86b35c36500627fd994d5b32d657280681b1cd0b
buryk20 <73736907+buryk20@users.noreply.github.com>
2023-12-25 09:59:26 +0200

    addIndexSrc

---
+++ www/src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="assets/main.css">
+</head>
+<body>
+<main data-wrp-main-page class="wrp">
+    <div data-ball class="ball"></div>
+    <?php
+    echo "<h1 class='title'>Hello world</h1>";
+    ?>
+</main>
+
+<script src="assets/index.js"></script>
+</body>
+</html>

47024e8ab9b7fc2d836af54419e87810a3b75906
buryk20 <73736907+buryk20@users.noreply.github.com>
2024-01-17 11:35:42 +0200

    dz11

---
+++ www/public/dz11.php
@@ -0,0 +1,31 @@
+<?php
+//1. Написати програму на PHP, яка приймає з консолі аргументи, які введені, і записує їх в файл
+require_once('./index.php');
+$filePath =  APP_DIR . "/database/data.txt";
+$data = inputVal();
+
+function inputVal()
+{
+    echo 'Введите данные' . PHP_EOL;
+    $input = fgets(STDIN);
+
+    return $input;
+}
+
+function writeToFile($data, $filePath)
+{
+    $data = date("Y-m-d H:i:s") . ' ' . $data;
+    file_put_contents($filePath, $data, FILE_APPEND);
+}
+
+//2. Написати іншу програму, яка виводить з файлу логу останні аргументи попередньої програми, які були введені.
+
+function outputData($filePath)
+{
+    $lines = file($filePath, FILE_IGNORE_NEW_LINES);
+    return end($lines);
+}
+
+writeToFile($data, $filePath);
+echo outputData($filePath);
+
--- www/public/index.php
+++ www/public/index.php
@@ -1,34 +1,3 @@
-
 <?php
+require_once '../system/constants.php';
 
-    $integerNumber = 10;
-    $floatNumber = 10.0;
-    $stringValue = "10";
-    $booleanValue = true;
-    $arrayValue = [1, 2, 3];
-
-
-    echo "Nonsensitive Comparison: </br>";
-    var_dump('$integerNumber == $floatNumber', $integerNumber == $floatNumber);
-    echo '</br>';
-    var_dump('$stringValue == $integerNumber', $stringValue == $integerNumber);
-    echo '</br>';
-    var_dump('$booleanValue == $integerNumber', $booleanValue == $integerNumber);
-    echo '</br>';
-    var_dump('$arrayValue == $integerNumber', $arrayValue == $integerNumber);
-    echo '</br>';
-    echo '</br>';
-    echo '</br>';
-
-
-    echo "\nSensitive Comparison: </br>";
-    var_dump('$integerNumber === $floatNumber', $integerNumber === $floatNumber);
-    echo '</br>';
-    var_dump('$stringValue === $integerNumber', $stringValue === $integerNumber);
-    echo '</br>';
-    var_dump('$booleanValue === $integerNumber', $booleanValue === $integerNumber);
-    echo '</br>';
-    var_dump('$arrayValue === $integerNumber', $arrayValue === $integerNumber);
-    echo '</br>';
-
-?>
